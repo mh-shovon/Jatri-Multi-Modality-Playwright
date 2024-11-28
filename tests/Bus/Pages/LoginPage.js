012983819e622f235e6b01b79f19f181bce9f8dd
MEHEDI HASAN SHOVON <shovon33266@gmail.com>
2024-11-29 01:12:19 +0600

    Connect redis and collect OTP to submit

--- tests/Bus/Pages/LoginPage.js
+++ tests/Bus/Pages/LoginPage.js
@@ -4,15 +4,23 @@ class LoginPage{
         this.loginInButton = page.locator("a button span");
         this.userPhoneNumber = page.locator("#mobile");
         this.getOtpButton = page.getByText('Get OTP');
+        this.otpField = page.locator("#otp");
     }
 
     async openLoginPage() {
         await this.loginInButton.click();
     }
 
-    async loginWithValidCredentials(userPhoneNumber) {
+    async enterValidPhoneNumber(userPhoneNumber) {
         await this.userPhoneNumber.fill(userPhoneNumber);
+    }
+
+    async clickOnGetOtpButton() {
         await this.getOtpButton.click();
+    }
+
+    async enterValidOtp(userOtp) {
+        await this.otpField.fill(userOtp)
     }
 }
 module.exports = { LoginPage };
--- tests/Bus/TestFiles/Jatri-Multimodal.spec.js
+++ tests/Bus/TestFiles/Jatri-Multimodal.spec.js
@@ -1,6 +1,13 @@
 const { test, expect } = require('@playwright/test');
 const { ControllerPage } = require('../Controller/ControllerPage');
 const dataSet = JSON.parse(JSON.stringify(require('../JsonFiles/UserInfo.json')));
+const Redis = require('ioredis');
+
+const redis = new Redis({
+    host: '128.199.226.196',
+    port: 6379,
+    password: 'JfzjT!MtVDAJZSqSPFv5KWDERaxHm9EQDq9SsDQ0pKP4dh8iwVJ8a4ffBBIJI'
+});
 
 test('Test-1 :: Visit the website and login with valid OTP', async ({ page }) => {
     const controllerPage = new ControllerPage(page);
@@ -10,5 +17,22 @@ test('Test-1 :: Visit the website and login with valid OTP', async ({ page }) =>
 
     const loginPage = controllerPage.getLoginPage();
     await loginPage.openLoginPage();
-    await loginPage.loginWithValidCredentials(dataSet.userPhoneNumber);
+    await loginPage.enterValidPhoneNumber(dataSet.userPhoneNumber);
+    await loginPage.clickOnGetOtpButton();
+    console.log('Waiting for OTP...');
+    let otp;
+    while (!otp) {
+        otp = await redis.get('auth-mm:cache:01983285059');
+        otp = JSON.parse(otp);
+        if (!otp) {
+            await new Promise(resolve => setTimeout(resolve, 60 * 1000));
+        }
+    }
+    console.log(`Retrieved OTP: ${otp}`);
+
+    dataSet.userOtp = otp;
+
+    await loginPage.enterValidOtp(dataSet.userOtp);
+
+    redis.disconnect();
 });
