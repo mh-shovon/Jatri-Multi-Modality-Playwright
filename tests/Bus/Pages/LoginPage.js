ef00ff834e1f91ad8370804ae68c172b6cf69115
MEHEDI HASAN SHOVON <shovon33266@gmail.com>
2024-11-30 20:03:05 +0600

    Update login page

--- tests/Bus/Pages/LoginPage.js
+++ tests/Bus/Pages/LoginPage.js
@@ -1,6 +1,5 @@
 class LoginPage{
     constructor(page) {
-        this.page = page;
         this.loginInButton = page.locator("a button span");
         this.userPhoneNumber = page.locator("#mobile");
         this.getOtpButton = page.getByText('Get OTP');
